5d73d6f9fe0b3cbde1a1086c85d5e9556f656a1a
Thushal Shaminda <it20216146@my.sliit.lk>
2022-05-24 00:15:32 +0530

    staff register frontend created

--- App.jsx
+++ App.jsx
@@ -7,17 +7,21 @@ import { createRoot } from 'react-dom/client'
 import App from './src/components/main';
 import Header from './src/components/Header';
 import CreateMarking from './src/pages/createMarking'
+import Sidebar from './src/components/Sidebar';
+import StaffRegister from './src/components/staff/StaffRegister'
 
 
 createRoot(document.getElementById('app')).render(
     <BrowserRouter>
         <div>
             {/* <Header /> */}
+            
             <Routes>
                 <Route exact path='/' element={< App />} />
-                <Route path='/marking-schemes' element={< App />} >
+                    <Route path='/marking-schemes' element={< App />} >
                     <Route path='create' element={<CreateMarking />} />
                 </Route>
+                    <Route path='/register' element={<StaffRegister />} />
             </Routes>
 
         </div>
