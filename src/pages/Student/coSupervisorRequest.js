eb626bb25606c522539608d80098016f299cc32b
Akila Kavinda <akilakavinda909@gmail.com>
2022-06-04 11:19:34 +0530

    sidebr chnges

--- src/pages/Student/coSupervisorRequest.js
+++ src/pages/Student/coSupervisorRequest.js
@@ -11,7 +11,9 @@ export default function CoSupervisorRequest() {
     groupId: "",
   });
 
-  var ItNumber = "IT20211714";
+  const localToken = JSON.parse(localStorage.getItem("localToken"));
+  console.log(localToken.username);
+  var ItNumber = localToken.username;
   useEffect(() => {
     const fetchStudent = async () => {
       await axios
