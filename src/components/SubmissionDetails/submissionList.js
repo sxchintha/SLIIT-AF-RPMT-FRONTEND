b3bab0444986544322bc3fa11be3442e8137c3ca
Thushal Shaminda <it20216146@my.sliit.lk>
2022-06-05 13:24:50 +0530

    submission list error fixed

--- src/components/SubmissionDetails/submissionList.js
+++ src/components/SubmissionDetails/submissionList.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 import { MDBDataTableV5 } from 'mdbreact'
 
-import { getItems } from '../../components/SubmissionDetails/api/index'
+import { getItems } from '../../components/SubmissionDetails/api/index';
+
+import Sidebar from "../Sidebar";
+import Footer from "../Footer";
 
 function SubmissionList() {
 
@@ -51,16 +54,37 @@ function SubmissionList() {
 
     return (
         <div>
-            <MDBDataTableV5
-                hover
-                entriesOptions={[5, 10, 20, 25]}
-                entries={10}
-                pagesAmount={4}
-                data={datatable}
-                searchTop
-                searchBottom={false} />
+            <div className="container-fluid overflow-hidden">
+                <div className="row vh-100 overflow-auto">
+                    <Sidebar />
+
+                    <div className="col d-flex flex-column h-sm-100">
+                        <main className="row overflow-auto">
+                            <div className="col pt-4 ps-4">
+                                {/* Body */}
+                                <h2>Panel Details</h2>
+                                <hr />
+                               
+                                <div>
+                                    <MDBDataTableV5
+                                        hover
+                                        entriesOptions={[5, 10, 20, 25]}
+                                        entries={10}
+                                        pagesAmount={4}
+                                        data={datatable}
+                                        searchTop
+                                        searchBottom={false} />
+                                </div>
+
+                            </div>
+                        </main>
+                        <Footer />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default SubmissionList;
+
