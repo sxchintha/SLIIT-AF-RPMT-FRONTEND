df69d6f90c786fcb09aba53abfe8fbdf76752383
Akila Kavinda <akilakavinda909@gmail.com>
2022-06-05 20:01:29 +0530

    remove sidebar

--- src/pages/Student/studentRegister.js
+++ src/pages/Student/studentRegister.js
@@ -58,7 +58,7 @@ export default function StudentRegsiter() {
         .then(() => {
           console.log(newStudentDetails);
           alert("Registeration succesfull!!");
-          navigate("/student/dashboard");
+          navigate("/login");
         })
         .catch((e) => {
           alert(e.response.data.error);
@@ -69,7 +69,138 @@ export default function StudentRegsiter() {
   }
   return (
     <>
-      <div>
+      <div className="col d-flex flex-column h-sm-100">
+        <div className="col pt-4 ps-4">
+          <div className="container py-5 h-100">
+            <div className="row justify-content-center align-items-center h-100">
+              <div className="col-12 col-lg-9 col-xl-7">
+                <div className="card shadow-2-strong card-registration">
+                  <div className="card-body p-4 p-md-5">
+                    <center>
+                      <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">
+                        Student Registration Form
+                      </h3>
+                    </center>
+
+                    <form onSubmit={sendData}>
+                      <div className="row">
+                        <div className="col-md-6 mb-4">
+                          <div className="form-floating">
+                            <input
+                              type="text"
+                              id="name"
+                              name="name"
+                              placeholder="Student name"
+                              className="form-control"
+                              onChange={onChange}
+                            />
+                            <label
+                              className="ms-2 text-secondary"
+                              for="firstName"
+                            >
+                              Student Name
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                          <div className="form-floating">
+                            <input
+                              type="text"
+                              id="lastName"
+                              name="itNumber"
+                              placeholder="Last name"
+                              className="form-control"
+                              onChange={onChange}
+                              required
+                            />
+                            <label
+                              className="ms-2 text-secondary"
+                              for="lastName"
+                            >
+                              IT Number
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-md-6 mb-4 d-flex align-items-center w-100">
+                          <div className="form-floating datepicker w-100">
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="username"
+                              name="email"
+                              placeholder="Username"
+                              onChange={onChange}
+                            />
+                            <label
+                              for="username"
+                              className="ms-2 text-secondary"
+                            >
+                              Email
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-floating">
+                            <input
+                              type="password"
+                              id="password"
+                              name="password"
+                              placeholder="Password"
+                              className="form-control"
+                              onChange={onChange}
+                            />
+                            <label
+                              className="ms-2 text-secondary"
+                              for="password"
+                            >
+                              Password
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-floating">
+                            <input
+                              type="password"
+                              id="confirm-password"
+                              name="cpassword"
+                              placeholder="Confirm Password"
+                              className="form-control"
+                              onChange={onChange}
+                            />
+                            <label
+                              className="ms-2 text-secondary"
+                              for="confirm-password"
+                            >
+                              Confirm Password
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="mt-4 pt-2">
+                        <center>
+                          <input
+                            className="btn btn-primary btn-lg"
+                            type="submit"
+                            value="Register"
+                          />
+                        </center>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div>
         <div className="container-fluid overflow-hidden">
           <div className="row vh-100 overflow-auto">
             <Sidebar />
@@ -80,127 +211,7 @@ export default function StudentRegsiter() {
                   <div className="container py-5 h-100">
                     <div className="row justify-content-center align-items-center h-100">
                       <div className="col-12 col-lg-9 col-xl-7">
-                        <div className="card shadow-2-strong card-registration">
-                          <div className="card-body p-4 p-md-5">
-                            <center>
-                              <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">
-                                Student Registration Form
-                              </h3>
-                            </center>
-
-                            <form onSubmit={sendData}>
-                              <div className="row">
-                                <div className="col-md-6 mb-4">
-                                  <div className="form-floating">
-                                    <input
-                                      type="text"
-                                      id="name"
-                                      name="name"
-                                      placeholder="Student name"
-                                      className="form-control"
-                                      onChange={onChange}
-                                    />
-                                    <label
-                                      className="ms-2 text-secondary"
-                                      for="firstName"
-                                    >
-                                      Student Name
-                                    </label>
-                                  </div>
-                                </div>
-                                <div className="col-md-6 mb-4">
-                                  <div className="form-floating">
-                                    <input
-                                      type="text"
-                                      id="lastName"
-                                      name="itNumber"
-                                      placeholder="Last name"
-                                      className="form-control"
-                                      onChange={onChange}
-                                      required
-                                    />
-                                    <label
-                                      className="ms-2 text-secondary"
-                                      for="lastName"
-                                    >
-                                      IT Number
-                                    </label>
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div className="row">
-                                <div className="col-md-6 mb-4 d-flex align-items-center w-100">
-                                  <div className="form-floating datepicker w-100">
-                                    <input
-                                      type="text"
-                                      className="form-control"
-                                      id="username"
-                                      name="email"
-                                      placeholder="Username"
-                                      onChange={onChange}
-                                    />
-                                    <label
-                                      for="username"
-                                      className="ms-2 text-secondary"
-                                    >
-                                      Email
-                                    </label>
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div className="row">
-                                <div className="col-md-6 mb-4 pb-2">
-                                  <div className="form-floating">
-                                    <input
-                                      type="password"
-                                      id="password"
-                                      name="password"
-                                      placeholder="Password"
-                                      className="form-control"
-                                      onChange={onChange}
-                                    />
-                                    <label
-                                      className="ms-2 text-secondary"
-                                      for="password"
-                                    >
-                                      Password
-                                    </label>
-                                  </div>
-                                </div>
-                                <div className="col-md-6 mb-4 pb-2">
-                                  <div className="form-floating">
-                                    <input
-                                      type="password"
-                                      id="confirm-password"
-                                      name="cpassword"
-                                      placeholder="Confirm Password"
-                                      className="form-control"
-                                      onChange={onChange}
-                                    />
-                                    <label
-                                      className="ms-2 text-secondary"
-                                      for="confirm-password"
-                                    >
-                                      Confirm Password
-                                    </label>
-                                  </div>
-                                </div>
-                              </div>
-
-                              <div className="mt-4 pt-2">
-                                <center>
-                                  <input
-                                    className="btn btn-primary btn-lg"
-                                    type="submit"
-                                    value="Register"
-                                  />
-                                </center>
-                              </div>
-                            </form>
-                          </div>
-                        </div>
+                        <div className="card shadow-2-strong card-registration"></div>
                       </div>
                     </div>
                   </div>
@@ -212,7 +223,7 @@ export default function StudentRegsiter() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
